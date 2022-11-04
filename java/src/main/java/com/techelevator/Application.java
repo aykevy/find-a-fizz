fa916c9b674a81f72f9bece5e65443c973ae9058
Kevin <kevywhoo@gmail.com>
2022-11-04 03:26:25 -0500

    Added JPA and edited the POM file and application properties to work for JPA

--- java/src/main/java/com/techelevator/Application.java
+++ java/src/main/java/com/techelevator/Application.java
@@ -1,13 +1,47 @@
 package com.techelevator;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(BreweriesRepository breweriesRepository)
+    {
+        return args -> {
+            Breweries b = new Breweries(
+                    "Kevin Brewery OMEGALUL",
+                    "Good Type",
+                    "1234 Lakers Suck",
+                    "Los Angeles",
+                    "CA",
+                    "69420",
+                    "google.com",
+                    "123-456-7891",
+                    LocalDate.now(),
+                    LocalDate.now(),
+                    "USA",
+                    null,
+                    null,
+                    null,
+                    null,
+                    "LA",
+                    "kevins",
+                    "brewin");
+            //Save example.
+            breweriesRepository.save(b);
+        };
     }
 
 }
---
+++ java/src/main/java/com/techelevator/BreweriesRepository.java
@@ -0,0 +1,7 @@
+package com.techelevator;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BreweriesRepository extends JpaRepository<Breweries, Long> {
+
+}
