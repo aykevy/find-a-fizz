3fb24e360f2031cabab1c5f2d10a6721b3e44ca5
Kevin <kevywhoo@gmail.com>
2022-11-06 08:27:02 -0600

    Added beer sql files, added 'add and update' methods for brewery

--- java/src/main/java/com/techelevator/Application.java
+++ java/src/main/java/com/techelevator/Application.java
@@ -1,11 +1,7 @@
 package com.techelevator;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class Application {
@@ -15,31 +11,4 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner(BreweryRepository breweryRepository)
-    {
-        return args -> {
-            Brewery b = new Brewery(
-                    "Kevin Test ",
-                    "Good Type",
-                    "1234 Lakers Suck",
-                    "Los Angeles",
-                    "CA",
-                    "69420",
-                    "google.com",
-                    "123-456-7891",
-                    LocalDate.now(),
-                    LocalDate.now(),
-                    "USA",
-                    null,
-                    null,
-                    null,
-                    null,
-                    "LA",
-                    "kevins",
-                    "brewin");
-            //Save example.
-            breweryRepository.save(b);
-        };
-    }
 }
---
+++ java/src/main/java/com/techelevator/Beer.java
@@ -0,0 +1,62 @@
+package com.techelevator;
+
+import javax.persistence.*;
+
+@Entity(name = "Beer")
+public class Beer {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    Long id;
+
+    @Column(name = "name", columnDefinition = "TEXT")
+    String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    String description;
+
+    @Column(name = "image_url", columnDefinition = "TEXT")
+    String imageUrl;
+
+    public Beer() {}
+
+    public Beer(Long id, String name, String description, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImgUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
