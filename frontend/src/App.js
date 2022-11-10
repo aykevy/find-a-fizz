e0ddf9140b1f9c48720f96b1c65f84f5f1cd2f03
Jacob Lay <erwinscatlives@gmail.com>
2022-11-09 19:33:11 -0600

    Finished Styling of Brew Cards/get all brews

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,44 @@
 import Main from './Components/Main/Main'
+import React from 'react'
 import {Provider} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
 import {ConfigureStore} from './Redux/configureStore'
 
 import Beer from './Components/Beer/Beer'
+import { axios } from './Redux/actionTypes'
 
 const store = ConfigureStore();
 
 function App() {
+  const [allBeers,setAllBeers] = React.useState([]);
+
+  const handleCLick=()=>{
+    axios.get('/beers').then(function (response){
+     setAllBeers(response.data);
+     console.log(response.data)
+ })
+ }
+  
 
   return (
     <Provider store={store}>
       <BrowserRouter>
-  
+        <>
+        <button onClick={() => handleCLick()}>Click to see beers</button>
         {/* <Main/> */}
         {/*Comment Main and uncomment beer to see random beer example work, run backend on intelij then run this on VS */}
-        {<Beer/>}
+        <div className='beer--list' >
+        {allBeers.map(beer =>{
+          return(
+          <Beer beer={beer} className='beer--element'/>
+          )
+        })}
+        </div>
+        </>
       </BrowserRouter>
     </Provider>
   );
+  
 }
 
 export default App;
--- frontend/src/Components/Beer/Beer.js
+++ frontend/src/Components/Beer/Beer.js
@@ -1,55 +1,45 @@
 import React from "react"
-import axios from "axios";
 import { Card, CardImg, CardText, CardTitle,CardBody } from "reactstrap";
 import { Component } from "react";
-import useGenerateRandomColor from "../../Redux/useRandomColors";
-/* This is just a simple test for now using beer with id 2, this is just to show that connection is working. */
-const API_BEER = 'http://localhost:8080/beer/2'
 
 
 export default class Beer extends Component{
+    
     //constructor for Beer Comp State.
     constructor(props){
         super(props)
-
-        //state props match object in DB
-        this.state ={
-            id: "",
-            name: "",
-            description: "",
-            imageUrl: "",
-            backgroundColor: "",
-        }
-        //binding this to mean this object in state
-        this.handleCLick = this.handleCLick.bind(this);
+        console.log(props)
     }
-
-    handleCLick(){
-        //call Axios to get beer API, then take response and set the state of the object to this( obj on  line 23)
-        // then bind those two objects to each other as this.
-        axios.get(API_BEER).then(function (response){
-            this.setState(response.data);}.bind(this))
-        }
     
-
     render(){
-        //Since we are using states we need a class comp, and since it's a class comp not funct comp we must render and return.
+        //Deconstruction of Props
+        const {
+            beer: {
+                    description,
+                    id,
+                    name,
+                    imageUrl
+                    },
+                  
+          } = this.props;
+          
     return(
         <div className = 'beer--card--set'>
-            <h1>TEST BEER OMEGALUL</h1>
-            <button onClick={() => this.handleCLick()}>Click to see beer with id 2</button>
-            <Card className = 'beer--card--ind' style={{backgroundColor:'lightblue'}}>
-                <CardImg top src = {this.state.imageUrl} alt = {this.state.name} className = 'beer--image'/>
+            <Card className="beer--card" key = {id}>
+                <CardImg top src = {imageUrl} alt = {name} />
                 <CardBody>
-                    <CardTitle className = 'beer--name'>{this.state.name}</CardTitle>
-                    <CardText className = 'beer--desc'>{this.state.description}</CardText>
+                    <CardTitle className = 'beer--card--name'>{name}</CardTitle>
+                    <CardText className = 'beer--card--desc'>{description}</CardText>
                 </CardBody>
 
             </Card>
+            
         </div>
+    //    </div>
     )
   }
 }
+
 
     /*This is the javascript way, will change later to react way using states*/
     // const beer_name = document.getElementById("beer--name");
