1ee9ab692cd01c2c7347550735e7f30a7ce7d437
Kevin <kevywhoo@gmail.com>
2022-11-30 13:32:43 -0600

    Pushed, nothing new tho

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,26 @@ import {Provider} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
 import {ConfigureStore} from './Redux/configureStore'
 
+
+
 const store = ConfigureStore();
 
 function App() {
  
   return (
+    
+    
     <Provider store={store}>
       <BrowserRouter>
         <Main/>
       </BrowserRouter>
     </Provider>
+    
+    
   );
   
 }
 
 export default App;
+
+//6aa188b6ee6448d07c753944a5303acd
