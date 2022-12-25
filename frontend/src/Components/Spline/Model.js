4d06f3030dd50403c878c48c2c3f5eef7e286d38
Kevin <kevywhoo@gmail.com>
2022-12-25 07:45:56 -0600

    Added Updates To Spline

--- frontend/src/Components/Spline/Model.js
+++ frontend/src/Components/Spline/Model.js
@@ -1,7 +1,25 @@
 import Spline from '@splinetool/react-spline';
+import {useState, useEffect} from 'react'
+import ClipLoader from "react-spinners/ClipLoader"
 
 export default function Model() {
+    const [loading, setLoading] = useState(false)
+    useEffect(() => {
+            setLoading(true)
+            setTimeout(() => {
+                setLoading(false)
+            }, 3000)
+
+    }, [])
+
     return (
-        <Spline className='spline' scene="scene.splinecode"/>
+        <div className='spline'>
+            {
+                loading ? <ClipLoader color={'black'} loading={loading} size={150} />
+                    : <Spline scene="https://prod.spline.design/xStq8NcwCkcvOFg7/scene.splinecode"/>
+            }
+        </div>
     )
+   
 }
+
